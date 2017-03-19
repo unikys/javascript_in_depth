298833b82cf2994c05a3b295724263a7943caad8
Sung-ihk Yang <sungihky@amazon.com>
2017-03-19 08:27:36 -0700

    [Chapter 5] Design pattern - Error fix from reader

--- chapter-5/5-179_setting_listener_separately.js
+++ chapter-5/5-179_setting_listener_separately.js
@@ -3,7 +3,7 @@
     var x, y, cell;
     for (x = 0; x < 100; x++) {
         for (y = 0; y < 100; y++) {
-            cell = document.getElementById("cell" + ("0" + x).substring(-2) + ("0" + y).substring(-2));
+            cell = document.getElementById("cell" + ("0" + x).substr(-2) + ("0" + y).substr(-2));
             cell.onmouseover = function () {
                 this.style.backgroundColor = "#EEEEEE";
             };
--- chapter-5/5-180_setting_listener_delegate.js
+++ chapter-5/5-180_setting_listener_delegate.js
@@ -2,18 +2,21 @@
 (function () {
     var wrapper = document.getElementById("tableWrapper");
     wrapper.addEventListener("mouseover", function () {
-        if (this.id && this.id.indexOf("cell") > -1) {
-            this.style.backgroundColor = "#EEEEEE";
+        var target = event.target || event.srcElement;
+        if (target.id && target.id.indexOf("cell") > -1) {
+            target.style.backgroundColor = "#EEEEEE";
         }
     }, true);
     wrapper.addEventListener("mouseout", function () {
-        if (this.id && this.id.indexOf("cell") > -1) {
-            this.style.backgroundColor = "#FFFFFF";
+        var target = event.target || event.srcElement;
+        if (target.id && target.id.indexOf("cell") > -1) {
+            target.style.backgroundColor = "#FFFFFF";
         }
     }, true);
     wrapper.addEventListener("click", function () {
-        if (this.id && this.id.indexOf("cell") > -1) {
-            alert(this.innerHTML);
+        var target = event.target || event.srcElement;
+        if (target.id && target.id.indexOf("cell") > -1) {
+            alert(target.innerHTML);
         }
     }, true);
 }());
